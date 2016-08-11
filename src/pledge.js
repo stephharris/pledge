bfa020299d03353dfbc523a30163921e92eea642
Stephanie Harris <310srh@gmail.com>
2016-08-11 15:02:28 -0400

    pledge stuff

--- src/pledge.js
+++ src/pledge.js
@@ -4,7 +4,56 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+var $Promise = function(){
+  this._state = 'pending';
+  this._value = '';
+  this._handlerGroups = [];
+}
 
+$Promise.prototype.then = function(suc, err){
+  var obj = {};
+  if(typeof suc === 'function'){
+    obj.successCb = suc;
+  }else{
+    obj.successCb = null;
+  }
+  if(typeof err === 'function'){
+    obj.errorCb = err;
+  }else{
+    obj.errorCb = null;
+  }
+  this._handlerGroups.push(obj);
+}
+
+var Deferral = function(){
+  this.$promise = new $Promise();
+}
+
+Deferral.prototype.resolve = function(data){
+  if(this.$promise._state  === 'pending'){
+    this.$promise._value = data;
+    this.$promise._state = 'resolved';
+  }
+}
+
+Deferral.prototype.reject = function(err){
+  if(this.$promise._state  === 'pending'){
+    this.$promise._value = err;
+    this.$promise._state = 'rejected';
+  }
+}
+
+
+
+
+
+var defer = function(){
+ return new Deferral()
+}
+
+//Deferral.prototype.$promise =
+
+console.log('#############', defer());
 
 
 
